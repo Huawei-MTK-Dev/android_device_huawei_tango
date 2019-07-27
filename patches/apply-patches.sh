1117e9469a9880762f2a8100120379d101d3509f
ReallySnow <reallysnow233@gmail.com>
2019-07-27 11:33:35 +0800

    fix patches

--- patches/apply-patches.sh
+++ patches/apply-patches.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 cd ../../../..
+echo install 1 patch
 cd frameworks/av
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch 
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/frameworks_av/0001-Disable-usage-of-get_capture_position.patch 
 cd ../..
+echo install 2 patch
 cd frameworks/native
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/frameworks_native_ui.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/frameworks_native_ui.patch
 cd ../..
+echo install 3 patch
 cd system/core
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/system_core/0001-Remove-CAP_SYS_NICE-from-surfaceflinger.patch
 cd ../..
 cd system/netd
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/system_netd/0002-Enable-Tethering.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/system_netd/0001-Revert-Don-t-start-tethering-if-IPv6-RPF-is-not-supp.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/system_netd/0002-Enable-Tethering.patch
 cd ../..
+echo install 4 patch
 cd system/sepolicy
-git apply --ignore-space-change --ignore-whitespace -v ../../device/HUAWEI/TAG_AL00/patches/system_sepolicy/0001-remove-ioctl-for-V29.patch
+git apply --ignore-space-change --ignore-whitespace -v ../../device/huawei/tango/patches/system_sepolicy/0001-Revert-back-to-version-29.patch
 cd ../..
 echo Patches Applied Successfully!
